927da82bce0fe8cb77a597bbfc258d723d86d46b
jonasstolt <101705622+jonasstolt@users.noreply.github.com>
2023-07-31 11:33:42 +0200

    Add files via upload
    Parse rss-links in terminal like: ~/scripts/rss_parser.pl https://www.host.com/rss/feed
    or use in Conky like ${execi 600 link-to-script link-to-rss-feed}

---
+++ rss_parser.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import feedparser
+import sys
+
+uri = sys.argv[1]            # URI för RSS-flöde
+lines = int(sys.argv[2])     # Antal rubriker
+titlenum = int(sys.argv[3])  # Antal extra rubriker
+
+# Scriptstart
+# Kräver en URI som ett minimum
+if not uri:
+    print("URI har inte specificerats.")
+    print("Kolla scriptets källkod")
+else:
+    # Sätt standardvärden om inga angivna
+    if not lines:
+        lines = 5
+    if not titlenum:
+        titlenum = 2
+
+    # Hämta RSS-flödet
+    feed = feedparser.parse(uri)
+
+    # Processa rubrikerna
+    for item in feed.entries[:lines+titlenum]:
+        title = item.title.split(" - ", 1)[-1]
+        title = title[15:]  # Ta bort de 15 första tecknen
+        # Sorry, det tar onödig plats
+        title = title.replace(" SWISH: 0720312394", "")
+        print(title)
---
+++ rss_parser.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'rss'
+require 'open-uri'
+
+uri = ARGV[0]            # URI för RSS-flöde
+lines = ARGV[1].to_i     # Antal rubriker
+titlenum = ARGV[2].to_i  # Antal extra rubriker
+
+# Skriptets start
+# Kräver en URI som ett minimum
+if uri.nil? || uri.empty?
+    puts "URI har inte specificerats."
+    puts "Kolla scriptets källkod"
+else
+    # Sätt standardvärden om inga angivna
+    lines = 5 if lines.zero?
+    titlenum = 2 if titlenum.zero?
+
+    # Hämta RSS-flödet
+    rss_content = URI.open(uri).read
+    feed = RSS::Parser.parse(rss_content)
+
+    # Processa rubrikerna
+    feed.items.first(lines + titlenum).each do |item|
+        title = item.title.split(" - ", 2).last
+        title = title[15..-1]  # Ta bort de 15 första tecknen
+        title = title.gsub(" SWISH: 0720312394", "") # Sorry, det tar onödig plats
+        puts titleS
+    end
+end
